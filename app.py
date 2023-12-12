e27e93b26581d27b38f7f0cf72b0897f7c26975b
Jia Rong Gan <jiaronggan21@gmail.com>
2023-12-12 14:33:39 -0600

    wrote models.py, updated requirements.txt, added new app forms file

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import connect_db, db, User
+from forms import RegisterForm, LoginForm, CSRFProtectForm
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "postgresql:///hashing_login")
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "abc123"
+
+connect_db(app)
+db.create_all()
+
+toolbar = DebugToolbarExtension(app)
+
