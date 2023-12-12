5fdc844024c9415d8eb9703d459c02cbb6637d17
Nicholas <nick.orsi1992@gmail.com>
2023-12-12 14:07:33 -0800

    Added routes for get '/' and '/register' with the related WTForm and html.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import connect_db, db, User
-from forms import RegisterForm, LoginForm, CSRFProtectForm
+from forms import
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
@@ -17,3 +17,14 @@ db.create_all()
 
 toolbar = DebugToolbarExtension(app)
 
+@app.get('/')
+def redirect_to_register():
+    """Redirect root url to '/resiter'"""
+
+    return redirect('/register')
+
+@app.route('/register', methods = ["GET", "POST"])
+def show_register_user():
+    """Renders the create user form"""
+
+    return render_template('register-user-form.html')
